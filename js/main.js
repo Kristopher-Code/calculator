55806b02b09c25c9d642d0d75a73ec086ae83216
Kristopher-Code <knicholcode@gmail.com>
2022-07-20 22:55:42 +0100

    Created the HTML skeleton. Styled a basic grid.

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+addition = (a, b) => {
+    return a + b;
+}
+
+subtract = (a, b) => {
+    return a - b;
+}
+
+multiply = (a, b) => {
+    return a * b;
+}
+
+divide = (a, b) => {
+    return a / b;
+}
